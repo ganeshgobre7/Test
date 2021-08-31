fb92e3f318ed86f320f8a49e5aceb08cd2ffcd1d
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-08-31 12:06:22 +0530

    All files added in remote server in master branch

---
+++ lengthconversion.sh
@@ -0,0 +1,6 @@
+echo        "Conversion of Inches to Feets"
+echo "-------------Note : 1 feet = 12 inches------------"
+ft=12
+read -p "Enter the Inches value : " inch
+conversion=$(echo "$inch $ft" | awk '{printf "%.2f \n", $1/$2}')
+echo "Coverted Inches in Feet is : $conversion "
