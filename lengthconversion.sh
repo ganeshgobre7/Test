f0c6018fda8e23111338dcb1301afeafe92e3b43
ganeshgobre7 <88813080+ganeshgobre7@users.noreply.github.com>
2021-08-31 12:15:33 +0530

    Conflict crate in line 8

--- lengthconversion.sh
+++ lengthconversion.sh
@@ -4,3 +4,5 @@ ft=12
 read -p "Enter the Inches value : " inch
 conversion=$(echo "$inch $ft" | awk '{printf "%.2f \n", $1/$2}')
 echo "Coverted Inches in Feet is : $conversion "
+
+conflict creeated in remote server in new branch
